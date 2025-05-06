4dd647515855eea6643e9768b055e62dc8c8571e
PanitanTeapujn <panitan.teap@bumail.net>
2025-05-06 16:23:29 +0700

    scripts base stats

--- Assets/Scritps/Character/Character.cs
+++ Assets/Scritps/Character/Character.cs
@@ -1,16 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+enum StatusEffectType
+{
+    None,
+    Poison,
+    Burn,
+    Freeze,
+    Stun,
+    Bleed
+}
 
+enum DamageType
+{
+    Normal,
+    Critical,
+    Magic,
+    Poison,
+    Burn,
+    Freeze,
+    Stun,
+    Bleed
+}
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [Header("Base Stats")]
+    private int currentHp; 
+    public int CurrentHp { get { return currentHp; } set { currentHp = value; } }
+    private int maxHp;
+    public int MaxHp { get { return maxHp; } set { currentHp = value; } }
+
+    private int attackDamage;
+    public int AttackDamage { get { return attackDamage; } set { attackDamage = value; } }
+
+    private int armor;
+    public int Armor { get { return armor; } set { armor = value; } }
+
+    private int moveSpeed;
+    public int MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
+    private float attackRange;
+    public float AttackRange { get { return attackRange; } set { attackRange = value; } }
+
+    private float attackCooldown;
+    public float AttackCooldown { get { return attackCooldown; } set { attackCooldown = value; } }
+
+    [Header("Physics")]
+    public Rigidbody rb;
+
+    [Header("Visual")]
+    public Renderer characterRenderer;
+    public Color originalColor;
+
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        characterRenderer = GetComponent<Renderer>();
+        if (characterRenderer != null)
+        {
+            originalColor = characterRenderer.material.color;
+        }
+
+    }
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
         
--- Assets/Scritps/Character/Hero/BloodKinght/BloodKinght.cs
+++ Assets/Scritps/Character/Hero/BloodKinght/BloodKinght.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : Character
+public class BloodKinght : Hero
 {
     // Start is called before the first frame update
     void Start()
