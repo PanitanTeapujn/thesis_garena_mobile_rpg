940a9ca1b6de0805bd3cd5c26df62415d87ecda9
PanitanTeapujn <panitan.teap@bumail.net>
2025-06-03 17:00:19 +0700

    all select character online

---
+++ Assets/Scritps/Character/Hero/Assassin/Assassin.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+public class Assassin : Hero
+{
+    // Start is called before the first frame update
+  
+}
---
+++ Assets/Scritps/Character/Hero/Berserker/Berserker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class Berserker : Hero
+{
+    // Start is called before the first frame update
+   
+}
