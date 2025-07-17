8bc8160aac26206074cc4356269393af140171e1
PanitanTeapujn <panitan.teap@bumail.net>
2025-07-17 19:53:36 +0700

    fix item trade

--- Assets/Scritps/FireBase/MultiCharacterPlayerData/MultiCharacterPlayerData.cs
+++ Assets/Scritps/FireBase/MultiCharacterPlayerData/MultiCharacterPlayerData.cs
@@ -560,7 +560,7 @@ public class SharedCurrencyData
     // Constructor
     public SharedCurrencyData()
     {
-        gold = 50000;  // เงินเริ่มต้น
+        gold = 5000;  // เงินเริ่มต้น
         gems = 500;    // เพชรเริ่มต้น
         maxGold = 999999999;
         maxGems = 999999;
